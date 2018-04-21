4f77509d4e29f9ca4ee56b100ca423026a3e24c7
Boris Makarevitch <boris.makarevitch@gmail.com>
2018-04-21 17:24:34 +0300

    Starting as command

--- pygas.py
+++ pygas.py
@@ -1,12 +1,7 @@
+#! /usr/bin/env python3
 import gi
 from gi.repository import GLib
 from pygas import App
-
-# win = Gtk.Window(title="Hello World")
-# win.show()
-# win.connect("destroy", Gtk.main_quit)
-# Gtk.main()
-
 
 if __name__ == '__main__':
     GLib.set_prgname('Pygas')
